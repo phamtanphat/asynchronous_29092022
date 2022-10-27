468853f6a315c80d44b9f4b25afeb47a646efcb7
phamtanphat <phatdroid94@gmail.com>
2022-10-27 18:05:56 +0700

    Initial commit

---
+++ lib/demo_future.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class DemoFuturePage extends StatefulWidget {
+
+  @override
+  State<DemoFuturePage> createState() => _DemoFuturePageState();
+}
+
+class _DemoFuturePageState extends State<DemoFuturePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Future"),
+      ),
+      body: Container(),
+    );
+  }
+}
